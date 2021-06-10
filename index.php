f4640d25e2665a216fc22d9b87bd5b184c2c3c6d
Gevorg Poghosyan <gevorg.poghosyan@ekeng.am>
2021-06-10 10:53:28 +0400

    rename upload button

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 
 <form enctype='multipart/form-data' action='upload.php' method='post'>
 
-    <label>Upload Product CSV file Here</label>
+    <label>Upload Employees CSV file Here</label>
 
     <input size='50' type='file' name='filename' required>
     </br>
-    <input type='submit' name='submit' value='Upload Products'>
+    <input type='submit' name='submit' value='Upload'>
 
 </form>
 
